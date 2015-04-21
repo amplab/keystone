f22909c1d90ad2c0d7158d9881554f193dc28591
Vaishaal Shankar <vaishaal@berkeley.edu>
2015-04-21 12:03:22 -0700

    implemented signed hellinger mapper, scaladocs and tests

---
+++ src/test/scala/stats/StatsSuite.scala
@@ -0,0 +1,20 @@
+package utils
+
+import breeze.linalg._
+
+import org.scalatest.{FlatSpec, FunSuite}
+import org.apache.spark.SparkContext
+import org.scalatest.matchers.ShouldMatchers
+
+import pipelines._
+
+class StatsSuite extends FlatSpec with LocalSparkContext with Logging with ShouldMatchers {
+  "signedHellingerMapper" should "take the signed square in an RDD" in {
+
+    sc = new SparkContext("local", "test")
+    val x = Seq(DenseVector(4.0, -16.0, 36.0, -1, -49))
+    val in = sc.parallelize(x)
+    val result = Stats.signedHellingerMapper(in).collect
+    result.toSeq should equal (Seq(DenseVector(2.0, -4.0, 6.0, -1, -7)))
+  }
+}

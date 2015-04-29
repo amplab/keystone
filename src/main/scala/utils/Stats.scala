7c3fec900e72c97de6c51781ba361d487bf83f7c
Vaishaal Shankar <vaishaal@berkeley.edu>
2015-04-28 17:50:39 -0700

    some style fixes and overloaded aboutEq

--- src/main/scala/utils/Stats.scala
+++ src/main/scala/utils/Stats.scala
@@ -1,67 +1,82 @@
 package utils
 
+import breeze.linalg._
+import breeze.numerics._
+import breeze.stats._
+import breeze.stats.distributions._
+
 import java.io.{FileWriter, File}
 import java.util.{Random => JRandom}
 import javax.imageio.ImageIO
 
 import org.apache.commons.io.FileUtils
 import org.apache.commons.math3.random.{RandomGenerator}
+import org.apache.spark.rdd.RDD
 
 import pipelines._
-import org.apache.spark.rdd.RDD
-import breeze.linalg._
-import breeze.numerics._
-import breeze.stats._
-import breeze.stats.distributions._
 
 import scala.reflect.ClassTag
 
 object Stats extends Serializable {
   /**
-   * Margin to use for comparing numerical values.
-   */
-  var thresh = 1e-8
+    * Margin to use for comparing numerical values.
+    */
+  val thresh = 1e-8
 
   /**
-   * Compares two numbers for approximate equality. Modify margin by setting Stats.thresh.
-   *
-   * @param a A number.
-   * @param b A second number.
-   * @return True if the two numbers are within `thresh` of each other.
-   */
-  def aboutEq(a: Double, b: Double): Boolean = {
+    * Compares two numbers for approximate equality
+    * @param a A number.
+    * @param b A second number.
+    * @param thresh equality threshold
+    * @return True if the two numbers are within `thresh` of each other.
+    */
+  def aboutEq(a: Double, b: Double, thresh: Double): Boolean = {
     math.abs(a-b) < thresh
   }
-
-  /**
-   * Compares two arrays for approximate equality. Modify margin by setting Stats.thresh.
-   *
-   * @param as A array of numbers.
-   * @param bs A second array of numbers.
-   * @return True if the two numbers are within `thresh` of each other.
-   */
-  def aboutEq(as: DenseVector[Double], bs: DenseVector[Double]): Boolean = {
-    abs(as-bs).toArray.forall(_ < thresh)
+  
+  def aboutEq(a: Double, b: Double): Boolean = {
+    Stats.aboutEq(a,b,thresh)
   }
 
   /**
-   * Compares two matrices for approximate equality.
-   *
-   * @param a A matrix.
-   * @param b A second matrix.
-   * @return True iff all corresponding elements of the two matrices are within `thresh` of each other.
-   */
-  def aboutEq(a: DenseMatrix[Double], b: DenseMatrix[Double]): Boolean = {
+    * Compares two arrays for approximate equality. Modify margin by setting Stats.thresh.
+    *
+    * @param as A array of numbers.
+    * @param bs A second array of numbers.
+    * @param thresh equality threshold
+    * @return True if the two numbers are within `thresh` of each other.
+    */
+  def aboutEq(as: DenseVector[Double], bs: DenseVector[Double], thresh: Double): Boolean = {
+    abs(as-bs).toArray.forall(_ < thresh)
+  }
+  
+  def aboutEq(as: DenseVector[Double], bs: DenseVector[Double]): Boolean = {
+    Stats.aboutEq(as,bs,thresh)
+  }
+
+  /**
+    * Compares two matrices for approximate equality.
+    *
+    * @param a A matrix.
+    * @param b A second matrix.
+    * @param thresh equality threshold
+    * @return True iff all corresponding elements of the two matrices are within `thresh` of each other.
+    */
+  def aboutEq(a: DenseMatrix[Double], b: DenseMatrix[Double], thresh: Double): Boolean = {
     require(a.rows == b.rows && a.cols == b.cols, "Matrices must be the same size.")
 
     abs(a-b).toArray.forall(_ < thresh)
   }
+
+  def aboutEq(a: DenseMatrix[Double], b: DenseMatrix[Double]): Boolean = {
+    Stats.aboutEq(a,b,thresh)
+  }
   /**
-   *  Apply power normalization: z <- sign(z)|z|^{\rho}
-   *  with \rho = \frac{1}{2}
-   *  This a "signed square root"
-   *  @param in  Input DenseVector[Double] RDD
-   */
+    *  Apply power normalization: z <- sign(z)|z|^{\rho}
+    *  with \rho = \frac{1}{2}
+    *  This a "signed square root"
+    *  @param in  Input DenseVector[Double] RDD
+    */
   def signedHellingerMapper(in: RDD[DenseVector[Double]]): RDD[DenseVector[Double]] = {
     in.map(x => {
       x.map(xi => math.signum(xi) * math.sqrt(math.abs(xi)))
@@ -86,7 +101,7 @@ object Stats extends Serializable {
     *  Generates a rows x cols Random matrix (dense) using Breeze's RandBasis (which takes in a prng)
     *  all values are Cauchy distributed double value with location 0.0 and scale 1.0
     */
-  def randMatrixCauchy(rows: Int, cols: Int, r: RandBasis = Rand): DenseMatrix[Double] = { 
+  def randMatrixCauchy(rows: Int, cols: Int, r: RandBasis = Rand): DenseMatrix[Double] = {
     tan(math.Pi*(DenseMatrix.rand(rows, cols, r.uniform) - 0.5))
   }
 }
--- src/test/scala/nodes/RandomSignNodeSuite.scala
+++ src/test/scala/nodes/RandomSignNodeSuite.scala
@@ -2,16 +2,19 @@ package nodes
 
 import pipelines._
 
-import org.scalatest.{FlatSpec, FunSuite}
-import pipelines.{Logging, LocalSparkContext}
-import org.apache.spark.SparkContext
-import org.scalatest.matchers.ShouldMatchers
 import breeze.linalg._
 import breeze.numerics._
 import breeze.stats._
 
-class RandomSignNodeSuite extends FlatSpec with LocalSparkContext with Logging with ShouldMatchers {
-  "RandomSignNode" should "flip signs according to a given vector" in {
+import org.apache.spark.SparkContext
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{FlatSpec, FunSuite}
+
+import pipelines.{Logging, LocalSparkContext}
+
+class RandomSignNodeSuite extends FunSuite with LocalSparkContext with Logging with ShouldMatchers {
+
+  test("RandomSignNode") {
     sc = new SparkContext("local", "test")
     val signs = DenseVector(1.0, -1.0, 1.0)
     val node = RandomSignNode(signs)
@@ -21,7 +24,7 @@ class RandomSignNodeSuite extends FlatSpec with LocalSparkContext with Logging w
     Seq(result(0)) should equal (Seq(DenseVector(1.0, -2.0, 3.0)))
   }
 
-  "RandomSignNode.create" should "create sane random sign vectors." in {
+  test("RandomSignNode.create") {
     val node = RandomSignNode.create(1000)
     
     node.signs.foreach(elt => assert(elt == -1.0 || elt == 1.0))

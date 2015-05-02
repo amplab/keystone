22af7de9385b7afacb828fd1f57f33a831ebd4cd
Evan Sparks <sparks@cs.berkeley.edu>
2015-05-01 18:33:19 -0700

    Adding grayscale transformer.

---
+++ src/main/scala/nodes/images/GrayScaler.scala
@@ -0,0 +1,15 @@
+package nodes.images
+
+import org.apache.spark.rdd.RDD
+import pipelines.Transformer
+import utils.{ImageUtils, Image}
+
+/**
+ * Converts an input images to NTSC-standard grayscale.
+ */
+class GrayScaler extends Transformer[Image,Image] {
+
+  def apply(in: RDD[Image]): RDD[Image] = {
+    in.map(ImageUtils.toGrayScale)
+  }
+}

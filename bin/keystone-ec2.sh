0a1f856f118fbb99a3bde62413da97c518f96bf8
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2015-06-02 19:33:20 -0700

    Use research branch

--- bin/keystone-ec2.sh
+++ bin/keystone-ec2.sh
@@ -9,6 +9,6 @@ $SPARK_EC2_DIR/spark-ec2 \
   --hadoop-major-version=2 \
   --spark-version=1.3.1 \
   --spark-ec2-git-repo=https://github.com/shivaram/spark-ec2 \
-  --spark-ec2-git-branch=keystone \
+  --spark-ec2-git-branch=keystone-imagenet-research \
   --copy-aws-credentials \
   $@

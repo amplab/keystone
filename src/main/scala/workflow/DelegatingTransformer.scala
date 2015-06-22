bdd58f20986f838a434c584f0ee4cd6caad793c3
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-06-21 22:31:38 -0700

    Response to pull request comments

--- src/main/scala/workflow/DelegatingTransformer.scala
+++ src/main/scala/workflow/DelegatingTransformer.scala
@@ -2,7 +2,11 @@ package workflow
 
 import org.apache.spark.rdd.RDD
 
-class DelegatingTransformer[T](override val label: String) extends TransformerNode[T] {
+/**
+ * A transformer used internally to apply the fit output of an Estimator onto data dependencies.
+ * Takes one fit dependency, and directly applies its transform onto the data dependencies.
+ */
+private[workflow] class DelegatingTransformer[T](override val label: String) extends TransformerNode[T] {
   def transform(dataDependencies: Seq[_], fitDependencies: Seq[TransformerNode[_]]): T = fitDependencies.head.transform(dataDependencies, Seq()).asInstanceOf[T]
 
   def transformRDD(dataDependencies: Seq[RDD[_]], fitDependencies: Seq[TransformerNode[_]]): RDD[T] = fitDependencies.head.transformRDD(dataDependencies, Seq()).asInstanceOf[RDD[T]]

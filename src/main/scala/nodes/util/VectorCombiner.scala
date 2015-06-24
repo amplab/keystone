73d5d4df9bef6b41575a5d507e61868d9cef6020
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-06-23 19:25:34 -0700

    Response to pull request comments

--- src/main/scala/nodes/util/VectorCombiner.scala
+++ src/main/scala/nodes/util/VectorCombiner.scala
@@ -8,6 +8,7 @@ import scala.reflect.ClassTag
 /**
  * Concats a Seq of DenseVectors into a single DenseVector.
  */
-case class VectorCombiner[T : ClassTag]()(implicit zero: breeze.storage.Zero[T]) extends Transformer[Seq[DenseVector[T]], DenseVector[T]] {
+case class VectorCombiner[T : ClassTag]()(implicit zero: breeze.storage.Zero[T])
+    extends Transformer[Seq[DenseVector[T]], DenseVector[T]] {
   def apply(in: Seq[DenseVector[T]]): DenseVector[T] = DenseVector.vertcat(in:_*)
 }
--- src/main/scala/workflow/Pipeline.scala
+++ src/main/scala/workflow/Pipeline.scala
@@ -1,51 +1,52 @@
 package workflow
 
 import org.apache.spark.rdd.RDD
-import pipelines.Logging
 
-import scala.collection.mutable
 import scala.reflect.ClassTag
 
+/**
+ * A Pipeline takes data as input (single item or an RDD), and outputs some transformation
+ * of that data. Internally, a Pipeline is represented by a DAG of [[Node]]s with two types of
+ * edges: One representing the data flow of RDDs through the DAG (for both transformations & for
+ * [[Estimator]] fitting), and one representing which [[Transformer]]s are output by which
+ * Estimators.
+ *
+ * @tparam A type of the data this Pipeline expects as input
+ * @tparam B type of the data this Pipeline outputs
+ */
 trait Pipeline[A, B] {
+  /**
+   * The nodes found in this Pipeline (index is the id)
+   */
   private[workflow] val nodes: Seq[Node]
+
+  /**
+   * Edges representing data flow through the DAG.
+   * (An edge points towards a node whose output data this node takes as input data)
+   * The indices of these edges map to the node indices.
+   */
   private[workflow] val dataDeps: Seq[Seq[Int]]
+
+  /**
+   * Edges pointing towards the Estimators a given Transformer is fit by.
+   * (Only non-empty for Transformer nodes)
+   * The indices of these edges map to the node indices.
+   */
   private[workflow] val fitDeps: Seq[Seq[Int]]
+
+  /**
+   * Index of the last node in the DAG before the transformed data is returned.
+   */
   private[workflow] val sink: Int
-
-  /** validates (returns an exception if false) that
-    - nodes.size = dataDeps.size == fitDeps.size
-
-    - there is a valid sink
-    - data nodes must have no deps
-    - estimators may not have fit deps, must have data deps
-    - transformers must have data deps, allowed to have fit deps
-
-    - data deps may not point at estimators
-    - fit deps may only point to estimators
-    */
-  private[workflow] def validate(): Unit = {
-    require(nodes.size == dataDeps.size && nodes.size == fitDeps.size, "nodes.size must equal dataDeps.size and fitDeps.size")
-    require(sink == Pipeline.SOURCE || (sink >= 0 && sink < nodes.size), "Sink must point at a valid node")
-
-    val nodeTuples = nodes.zip(dataDeps).zip(fitDeps).map(x => (x._1._1, x._1._2, x._2))
-    require(nodeTuples.forall(x => if (x._1.isInstanceOf[DataNode] && (x._2.nonEmpty || x._3.nonEmpty)) false else true), "DataNodes may not have dependencies")
-    require(nodeTuples.forall(x => if (x._1.isInstanceOf[EstimatorNode] && x._3.nonEmpty) false else true), "Estimators may not have fit dependencies")
-    require(nodeTuples.forall(x => if (x._1.isInstanceOf[EstimatorNode] && x._2.isEmpty) false else true), "Estimators must have data dependencies")
-    require(nodeTuples.forall(x => if (x._1.isInstanceOf[TransformerNode[_]] && x._2.isEmpty) false else true), "Transformers must have data dependencies")
-
-    require(dataDeps.forall(_.forall(x => !nodes(x).isInstanceOf[EstimatorNode])), "Data dependencies may not point at Estimators")
-    require(fitDeps.forall(_.forall(x => nodes(x).isInstanceOf[EstimatorNode])), "Fit dependencies may only point at Estimators")
-
-    /*TODO: Validate that
-    - there is a data path from sink to in
-    - there are no fit paths from sink to in
-    */
-  }
 
   def apply(in: A): B
 
   def apply(in: RDD[A]): RDD[B]
 
+  /**
+   * Chains a pipeline onto the end of this one, producing a new pipeline.
+   * @param next the pipeline to chain
+   */
   final def andThen[C](next: Pipeline[B, C]): Pipeline[A, C] = {
     val nodes = this.nodes ++ next.nodes
     val dataDeps = this.dataDeps ++ next.dataDeps.map(_.map {
@@ -71,13 +72,23 @@ trait Pipeline[A, B] {
 
     val fittedTransformer = Pipeline[B, C](newNodes, newDataDeps, newFitDeps, newSink)
     val totalOut = this andThen fittedTransformer
-    new PipelineWithFittedTransformer(totalOut.nodes, totalOut.dataDeps, totalOut.fitDeps, totalOut.sink, fittedTransformer)
+    new PipelineWithFittedTransformer(
+      totalOut.nodes,
+      totalOut.dataDeps,
+      totalOut.fitDeps,
+      totalOut.sink,
+      fittedTransformer)
   }
 
-  final def andThen[C, L](est: LabelEstimator[B, C, L], data: RDD[A], labels: RDD[L]): PipelineWithFittedTransformer[A, B, C] = {
+  final def andThen[C, L](est: LabelEstimator[B, C, L], data: RDD[A], labels: RDD[L]):
+  PipelineWithFittedTransformer[A, B, C] = {
     val transformerLabel = est.label + ".fit"
 
-    val newNodes = this.nodes :+ est :+ DataNode(data) :+ DataNode(labels) :+ new DelegatingTransformer[C](transformerLabel)
+    val newNodes = this.nodes :+
+        est :+
+        DataNode(data) :+
+        DataNode(labels) :+
+        new DelegatingTransformer[C](transformerLabel)
     val newDataDeps = this.dataDeps.map(_.map {
       x => if (x == Pipeline.SOURCE) this.nodes.size + 1 else x
     }) :+ Seq(this.sink, this.nodes.size + 2) :+ Seq() :+ Seq() :+ Seq(Pipeline.SOURCE)
@@ -86,10 +97,18 @@ trait Pipeline[A, B] {
 
     val fittedTransformer = Pipeline[B, C](newNodes, newDataDeps, newFitDeps, newSink)
     val totalOut = this andThen fittedTransformer
-    new PipelineWithFittedTransformer(totalOut.nodes, totalOut.dataDeps, totalOut.fitDeps, totalOut.sink, fittedTransformer)
+    new PipelineWithFittedTransformer(
+      totalOut.nodes,
+      totalOut.dataDeps,
+      totalOut.fitDeps,
+      totalOut.sink,
+      fittedTransformer)
   }
 
 
+  /**
+   * @return A graphviz dot representation of this pipeline
+   */
   final def toDOTString: String = {
     val nodeLabels: Seq[String] = "-1 [label=\"In\" shape=\"Msquare\"]" +: nodes.zipWithIndex.map {
       case (data: DataNode, id)  => s"$id [label=${'"' + data.label + '"'} shape=${"\"box\""} style=${"\"filled\""}]"
@@ -130,7 +149,17 @@ object Pipeline {
    * @tparam T The type of input to take
    */
   def apply[T](): Pipeline[T, T] = new ConcretePipeline(Seq(), Seq(), Seq(), SOURCE)
-  private[workflow] def apply[A, B](nodes: Seq[Node], dataDeps: Seq[Seq[Int]], fitDeps: Seq[Seq[Int]], sink: Int): Pipeline[A, B] = new ConcretePipeline(nodes, dataDeps, fitDeps, sink)
+
+  /**
+   * Constructs a Pipeline with the given DAG specification
+   */
+  private[workflow] def apply[A, B](
+    nodes: Seq[Node],
+    dataDeps: Seq[Seq[Int]],
+    fitDeps: Seq[Seq[Int]],
+    sink: Int): Pipeline[A, B] = {
+    new ConcretePipeline(nodes, dataDeps, fitDeps, sink)
+  }
 
   /**
    * Produces a pipeline that when given an input,
@@ -159,78 +188,6 @@ object Pipeline {
     val newSink = newNodes.size - 1
     Pipeline(newNodes, newDataDeps, newFitDeps, newSink)
   }
-
-}
-
-/**
- * An implementation of [[Pipeline]] that explicitly specifies the Pipline DAG as vals,
- * With and apply method that works via traversing the DAG graph and executing it.
- */
-private[workflow] class ConcretePipeline[A, B](
-  private[workflow] override val nodes: Seq[Node],
-  private[workflow] override val dataDeps: Seq[Seq[Int]],
-  private[workflow] override val fitDeps: Seq[Seq[Int]],
-  private[workflow] override val sink: Int) extends Pipeline[A, B] with Logging {
-
-  private val fitCache: Array[Option[TransformerNode[_]]] = nodes.map(_ => None).toArray
-  private val dataCache: mutable.Map[(Int, RDD[_]), RDD[_]] = new mutable.HashMap()
-
-  final private[workflow] def fitEstimator(node: Int): TransformerNode[_] = fitCache(node).getOrElse {
-    nodes(node) match {
-      case _: DataNode =>
-        throw new RuntimeException("Pipeline DAG error: Cannot have a fit dependency on a DataNode")
-      case _: TransformerNode[_] =>
-        throw new RuntimeException("Pipeline DAG error: Cannot have a data dependency on a Transformer")
-      case estimator: EstimatorNode =>
-        val nodeDataDeps = dataDeps(node).map(x => rddDataEval(x, null))
-        logInfo(s"Fitting '${estimator.label}' [$node]")
-        val fitOut = estimator.fit(nodeDataDeps)
-        fitCache(node) = Some(fitOut)
-        logInfo(s"Finished fitting '${estimator.label}' [$node]")
-        fitOut
-    }
-  }
-
-  final private[workflow] def singleDataEval(node: Int, in: A): Any = {
-    if (node == Pipeline.SOURCE) {
-      in
-    } else {
-      nodes(node) match {
-        case transformer: TransformerNode[_] =>
-          val nodeFitDeps = fitDeps(node).map(fitEstimator)
-          val nodeDataDeps = dataDeps(node).map(x => singleDataEval(x, in))
-          transformer.transform(nodeDataDeps, nodeFitDeps)
-        case _: DataNode =>
-          throw new RuntimeException("Pipeline DAG error: came across an RDD data dependency when trying to do a single item apply")
-        case _: EstimatorNode =>
-          throw new RuntimeException("Pipeline DAG error: Cannot have a data dependency on an Estimator")
-      }
-    }
-  }
-
-  final private[workflow] def rddDataEval(node: Int, in: RDD[A]): RDD[_] = {
-    if (node == Pipeline.SOURCE) {
-      in
-    } else {
-      dataCache.getOrElse((node, in), {
-        nodes(node) match {
-          case DataNode(rdd) => rdd
-          case transformer: TransformerNode[_] =>
-            val nodeFitDeps = fitDeps(node).map(fitEstimator)
-            val nodeDataDeps = dataDeps(node).map(x => rddDataEval(x, in))
-            val outputData = transformer.transformRDD(nodeDataDeps, nodeFitDeps)
-            dataCache((node, in)) = outputData
-            outputData
-          case _: EstimatorNode =>
-            throw new RuntimeException("Pipeline DAG error: Cannot have a data dependency on an Estimator")
-        }
-      })
-    }
-  }
-
-  override def apply(in: A): B = singleDataEval(sink, in).asInstanceOf[B]
-
-  override def apply(in: RDD[A]): RDD[B] = rddDataEval(sink, in).asInstanceOf[RDD[B]]
 }
 
 class PipelineWithFittedTransformer[A, B, C] private[workflow] (

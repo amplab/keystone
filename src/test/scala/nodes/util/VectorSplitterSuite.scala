fe5f84e71c6ec4e7bbfae98f4ca637106b36ea09
Evan Sparks <sparks@cs.berkeley.edu>
2016-01-20 11:20:20 -0800

    Making sure recombined vectors match input and fixing test formatting issues.

--- src/test/scala/nodes/util/VectorSplitterSuite.scala
+++ src/test/scala/nodes/util/VectorSplitterSuite.scala
@@ -1,6 +1,6 @@
 package nodes.util
 
-import breeze.linalg.DenseVector
+import breeze.linalg._
 import org.scalatest.FunSuite
 
 class VectorSplitterSuite extends FunSuite {
@@ -9,15 +9,32 @@ class VectorSplitterSuite extends FunSuite {
       bs <- Array(128, 256, 512, 1024, 2048);
       mul <- 0 to 2;
       off <- 0 to 20 by 5;
-      feats <- Array(Some(bs*mul+off), None)
+      feats <- Array(Some(bs*mul + off), None)
     ) {
       val sp = new VectorSplitter(bs, feats)
-      val vec = DenseVector.zeros[Double](bs*mul+off)
+      val vec = DenseVector.zeros[Double](bs*mul + off)
 
-      val expectedSplits = (bs*mul+off)/bs + (if ((bs*mul+off)%bs == 0) 0 else 1)
+      val expectedSplits = (bs*mul + off)/bs + (if ((bs*mul + off) % bs == 0) 0 else 1)
 
       assert(sp.splitVector(vec).length === expectedSplits,
         s"True length is ${sp.splitVector(vec).length}, expected length is ${expectedSplits}")
+    }
+  }
+
+  test("vector splitter maintains order") {
+    for (
+      bs <- Array(128, 256, 512, 1024, 2048);
+      mul <- 0 to 2;
+      off <- 0 to 20 by 5;
+      feats <- Array(Some(bs*mul + off), None)
+    ) {
+      val sp = new VectorSplitter(bs, feats)
+      val vec = rand(bs*mul + off)
+
+      val expectedSplits = (bs*mul + off) / bs + (if ((bs*mul+off) % bs == 0) 0 else 1)
+
+      assert(DenseVector.vertcat(sp.splitVector(vec):_*) === vec,
+        s"Recombinded split vector of length ${bs*mul + off} with block size $bs did not match its input")
     }
   }
 }

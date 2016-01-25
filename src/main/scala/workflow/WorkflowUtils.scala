65fd15f386881276e23fcff9fe6fbea4dc7293b5
Tomer Kaftan <tomer.kaftan@gmail.com>
2016-01-25 15:23:22 -0800

    Added an instructions to pipeline method

--- src/main/scala/workflow/WorkflowUtils.scala
+++ src/main/scala/workflow/WorkflowUtils.scala
@@ -1,7 +1,57 @@
 package workflow
 
+import scala.collection.mutable.ArrayBuffer
+
 
 object WorkflowUtils {
+  def instructionsToPipeline[A, B](instructions: Seq[Instruction]): Pipeline[A, B] = {
+    val nodes = new ArrayBuffer[Node]()
+    val dataDeps = new ArrayBuffer[Seq[Int]]()
+    val fitDeps = new ArrayBuffer[Option[Int]]()
+    val instructionIdToNodeId = scala.collection.mutable.Map.empty[Int, Int]
+    instructionIdToNodeId.put(Pipeline.SOURCE, Pipeline.SOURCE)
+
+    for (instruction <- 0 until instructions.length) {
+      instructions(instruction) match {
+        case est: EstimatorNode => Unit
+        case transformer: TransformerNode => Unit
+        case source: SourceNode => {
+          instructionIdToNodeId.put(instruction, nodes.length)
+          nodes.append(source)
+          dataDeps.append(Seq())
+          fitDeps.append(None)
+        }
+        case TransformerApplyNode(transformer, inputs) => {
+          instructions(transformer) match {
+            case transformerNode: TransformerNode => {
+              instructionIdToNodeId.put(instruction, nodes.length)
+              nodes.append(transformerNode)
+              dataDeps.append(inputs.map(instructionIdToNodeId.apply))
+              fitDeps.append(None)
+            }
+            case estimatorFitNode: EstimatorFitNode => {
+              instructionIdToNodeId.put(instruction, nodes.length)
+              // TODO: Get more reasonable label...
+              nodes.append(new DelegatingTransformerNode("Estimator fit"))
+              dataDeps.append(inputs.map(instructionIdToNodeId.apply))
+              fitDeps.append(Some(instructionIdToNodeId(transformer)))
+            }
+            case _ => throw new RuntimeException("Transformer apply instruction must point at a Transformer")
+          }
+        }
+        case EstimatorFitNode(est, inputs) => {
+          val estimatorNode = instructions(est).asInstanceOf[EstimatorNode]
+          instructionIdToNodeId.put(instruction, nodes.length)
+          nodes.append(estimatorNode)
+          dataDeps.append(inputs.map(instructionIdToNodeId.apply))
+          fitDeps.append(None)
+        }
+      }
+    }
+
+    new ConcretePipeline(nodes.toSeq, dataDeps.toSeq, fitDeps.toSeq, nodes.length - 1)
+  }
+
   def pipelineToInstructions[A, B](pipeline: Pipeline[A, B]): Seq[Instruction] = {
     val nodes = pipeline.nodes
     val dataDeps = pipeline.dataDeps

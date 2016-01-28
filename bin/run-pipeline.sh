b860d31b5c48c24fd646e4768e189984136d4913
Evan Sparks <sparks@cs.berkeley.edu>
2016-01-27 17:49:07 -0800

    Adding logic to set OMP_NUM_THREADS appropriately on master and executors in a variety of situations.

--- bin/run-pipeline.sh
+++ bin/run-pipeline.sh
@@ -6,6 +6,32 @@ FWDIR="$(cd `dirname $0`; pwd)"
 CLASS=$1
 shift
 
+# Set OMP_NUM_THREADS on workers and driver to something appropriate.
+# This is due to OpenBLAS not handling large numbers of cores very well.
+
+if [[ -z "$OMP_NUM_THREADS" ]]; then
+  # Determine number of cores. We assume that hyperthreading is enabled and thus divide cores by two.
+  unamestr=`uname`
+  if [[ unamestr -eq "Darwin" ]]; then
+    CORES=$((`sysctl -n hw.ncpu`/2))
+  elif [[ unamestr -eq "Linux" ]]; then
+    CORES=$((`cat /proc/cpuinfo | grep processor | wc -l`/2))
+  else # Windows,BSD? Do the safest thing.
+    CORES=1
+  fi 
+ 
+  # Set OMP_NUM_THREADS to MIN(32,CORES) to avoid stack smashing issues.
+  export OMP_NUM_THREADS=$(($CORES>32?32:$CORES))
+else
+  if [[ $OMP_NUM_THREADS -gt 32 ]]; then
+    echo 'Warning: setting OMP_NUM_THREADS > 32 may cause instability.'
+  fi
+fi
+
+if [[ -z "$EXECUTOR_OMP_NUM_THREADS" ]]; then
+  EXECUTOR_OMP_NUM_THREADS=1
+fi  
+
 if [[ -z "$SPARK_HOME" ]]; then
   echo "SPARK_HOME is not set, running pipeline locally"
   $FWDIR/run-main.sh $CLASS "$@"
@@ -22,6 +48,7 @@ else
     --driver-library-path $FWDIR/../lib \
     --conf spark.executor.extraLibraryPath=$FWDIR/../lib \
     --conf spark.executor.extraClassPath=$FWDIR/../target/scala-2.10/keystoneml-assembly-0.3.0-SNAPSHOT.jar \
+    --conf spark.executorEnv.OMP_NUM_THREADS=$EXECUTOR_OMP_NUM_THREADS \
     --driver-memory $KEYSTONE_MEM \
     target/scala-2.10/keystoneml-assembly-0.3.0-SNAPSHOT.jar \
     "$@"

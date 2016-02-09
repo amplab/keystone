3c35b34dca8ad184f16a29f598cff3de4aa5d673
Tomer Kaftan <tomer.kaftan@gmail.com>
2016-02-08 17:11:32 -0800

    made commonsparsefeatures use priority queue per-partition

--- src/main/scala/nodes/util/CommonSparseFeatures.scala
+++ src/main/scala/nodes/util/CommonSparseFeatures.scala
@@ -4,6 +4,7 @@ import breeze.linalg.SparseVector
 import org.apache.spark.rdd.RDD
 import workflow.Estimator
 
+import scala.collection.JavaConversions._
 import scala.reflect.ClassTag
 
 /**
@@ -27,7 +28,10 @@ case class CommonSparseFeatures[T : ClassTag](numFeatures: Int) extends Estimato
     }
   }
 
-  def merge(a: Array[(T, (Int, Long))], b: Array[(T, (Int, Long))]): Array[(T, (Int, Long))] = (a ++ b).sorted(ordering.reverse).take(numFeatures)
+  /** This method merges two seqs and keeps the top numFeatures */
+  def merge(a: Seq[(T, (Int, Long))], b: Seq[(T, (Int, Long))]): Seq[(T, (Int, Long))] = {
+    (a ++ b).sorted(ordering.reverse).take(numFeatures)
+  }
 
   override def fit(data: RDD[Seq[(T, Double)]]): SparseFeatureVectorizer[T] = {
     val featureOccurrences = data.flatMap(identity).zipWithUniqueId().map(x => (x._1._1, (1, x._2)))
@@ -36,8 +40,25 @@ case class CommonSparseFeatures[T : ClassTag](numFeatures: Int) extends Estimato
     val featureFrequenciesWithUniqueId = featureOccurrences.reduceByKey {
       (x, y) => (x._1 + y._1, Math.min(x._2, y._2))
     }
-    val mostCommonFeatures = featureFrequenciesWithUniqueId.glom().map(_.sorted(ordering.reverse).take(numFeatures)).treeReduce(merge).map(_._1)
+    val mapRDDs = featureFrequenciesWithUniqueId mapPartitions { items =>
+      // Priority keeps the largest elements, so let's reverse the ordering.
+      Iterator.single(takeOrdered(items, numFeatures)(ordering.reverse))
+    }
+    val mostCommonFeatures = mapRDDs.treeReduce(merge).map(_._1)
+
     val featureSpace = mostCommonFeatures.zipWithIndex.toMap
     new SparseFeatureVectorizer(featureSpace)
   }
+
+  /**
+   * Returns the first K elements from the input as defined by the specified implicit Ordering[T]
+   * and maintains the ordering.
+   */
+  def takeOrdered[T](input: Iterator[T], num: Int)(implicit ord: Ordering[T]): Seq[T] = {
+    val ordering = new com.google.common.collect.Ordering[T] {
+      override def compare(l: T, r: T) = ord.compare(l, r)
+    }
+    ordering.leastOf(asJavaIterator(input), num)
+  }
+
 }

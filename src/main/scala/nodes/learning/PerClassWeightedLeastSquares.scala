0a96cf316f97d7d3ac9bcf0fdfd72948d043feef
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2016-02-21 20:59:22 -0800

    Refactor weighted least squares code.

--- src/main/scala/nodes/learning/PerClassWeightedLeastSquares.scala
+++ src/main/scala/nodes/learning/PerClassWeightedLeastSquares.scala
@@ -1,6 +1,7 @@
 package nodes.learning
 
 import nodes.util.VectorSplitter
+import nodes.learning.internal.ReWeightedLeastSquaresSolver
 import workflow.LabelEstimator
 
 import scala.collection.mutable.ArrayBuffer
@@ -205,89 +206,18 @@ object PerClassWeightedLeastSquaresEstimator extends Logging {
       classWeights: RDD[Double],
       classMean: DenseVector[Double]): Seq[DenseVector[Double]] = {
 
-    val classLabelsZmMat = classLabelsZm.mapPartitions { iter =>
-      Iterator.single(DenseVector(iter.toArray))
-    }
-    val classWeightsMat = classWeights.mapPartitions { iter =>
-      Iterator.single(DenseVector(iter.toArray))
-    }
+    val (model, residual) = ReWeightedLeastSquaresSolver.trainWithL2(
+      blockSize,
+      numBlocks,
+      numIter,
+      lambda,
+      numFeatures,
+      1, // numClasses
+      trainingFeatureBlocks,
+      classLabelsZm.map(x => DenseVector(x)),
+      classWeights,
+      classMean)
 
-    var residual = classLabelsZmMat.map { l =>
-      DenseVector.zeros[Double](l.size)
-    }.cache()
-
-    // Initialize model to blockSize. This will be resized if its different
-    // inside the solve loop
-    val model = (0 until numBlocks).map { block =>
-      DenseVector.zeros[Double](blockSize)
-    }.toArray
-
-    val treeBranchingFactor = classWeights.context.getConf.getInt(
-      "spark.mlmatrix.treeBranchingFactor", 2).toInt
-    val depth = math.max(math.ceil(math.log(classLabelsZmMat.partitions.size) /
-        math.log(treeBranchingFactor)).toInt, 1)
-    val aTaCache = new Array[DenseMatrix[Double]](numBlocks)
-
-    (0 until numIter).foreach { pass =>
-      (0 until numBlocks).foreach { block =>
-        val aPart = trainingFeatureBlocks(block)
-        // Zero-mean the training features
-        val classMeanBlock = classMean(
-          block * blockSize until min((block+1) * blockSize, numFeatures))
-        val aPartMatZm = aPart.mapPartitions { iter =>
-          val mat = MatrixUtils.rowsToMatrix(iter)
-          Iterator.single( (mat(*, ::) - classMeanBlock) )
-        }.cache().setName("aPartMatZm")
-        aPartMatZm.count
-      
-        // Compute X.t * (diag(B) * X) if this the first iteration
-        if (pass == 0) {
-          val aTaComputed = MLMatrixUtils.treeReduce(aPartMatZm.zip(classWeightsMat).map { x =>
-            // Multiplication by diagonal is same as hadamard product per column
-            x._1.t * ( x._1(::, *) :* x._2 )
-          }, (a: DenseMatrix[Double], b: DenseMatrix[Double]) => a += b, depth=depth)
-          aTaCache(block) = aTaComputed
-          model(block) = DenseVector.zeros[Double](aTaComputed.rows)
-        }
-
-        val aTaBlock = aTaCache(block)
-        val modelBC = classWeightsMat.context.broadcast(model(block))
-
-        val aTbBlock = MLMatrixUtils.treeReduce(
-          aPartMatZm.zip(classLabelsZmMat.zip(residual.zip(classWeightsMat))).map { x =>
-            val featPart = x._1
-            val labelPart = x._2._1
-            val resPart = x._2._2._1
-            val weightPart = x._2._2._2
-           
-            // TODO(shivaram): This might generate a lot of GC ?
-            
-            // Remove B.*(X * wOld) from the residual
-            val resUpdated = resPart - (weightPart :* (featPart * modelBC.value))
-           
-            // Compute X.t * ((B .* Y) - residual)
-            val aTbPart = featPart.t * ((weightPart :* labelPart) - resUpdated)
-            aTbPart 
-          }, (a: DenseVector[Double], b: DenseVector[Double]) => a += b, depth = depth)
-
-        val newModel = (aTaBlock + (DenseMatrix.eye[Double](aTaBlock.rows) * lambda)) \ aTbBlock
-        val newModelBC = classWeights.context.broadcast(newModel)
-
-        model(block) = newModel
-
-        // Update the residual by adding B.*(X * wNew) and subtracting B.*(X * wOld)
-        val newResidual = aPartMatZm.zip(residual.zip(classWeightsMat)).map { part =>
-          val diffModel = newModelBC.value - modelBC.value
-          part._2._1 += (part._2._2 :* (part._1 * diffModel))
-          part._2._1
-        }.cache().setName("residual")
-        newResidual.count
-        residual.unpersist()
-        residual = newResidual
-
-        aPartMatZm.unpersist()
-      }
-    }
-    model
+    model.map(x => x(::, 0))
   }
 }

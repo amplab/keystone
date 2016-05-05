9b6ca677fa2a2c21b2512afcb9640f75b915de33
Tomer Kaftan <tomer.kaftan@gmail.com>
2016-05-05 15:30:10 -0700

    implement some of the methods

--- src/main/scala/workflow/graph/Pipeline.scala
+++ src/main/scala/workflow/graph/Pipeline.scala
@@ -2,8 +2,9 @@ package workflow.graph
 
 import org.apache.spark.rdd.RDD
 
-class GraphExecutor(private val graph: Graph, private val optimizer: Optimizer) {
-  private lazy val optimizedGraph: Graph = optimizer.execute(graph)
+class GraphExecutor(val graph: Graph, val optimizer: Option[Optimizer]) {
+  private lazy val optimizedGraph: Graph = optimizer.map(_.execute(graph)).getOrElse(graph)
+
   // Todo put comment: A result is unstorable if it implicitly depends on any source
   private lazy val unstorableResults: Set[GraphId] = {
     optimizedGraph.sources.foldLeft(Set[GraphId]()) {
@@ -49,22 +50,42 @@ trait GraphBackedExecution {
 }
 
 // A lazy representation of a pipeline output
-class PipelineDatumOut[T] extends GraphBackedExecution {
+class PipelineDatumOut[T](protected var executor: GraphExecutor, sink: SinkId) extends GraphBackedExecution {
   protected var sources = Seq()
-  protected var sinks = Seq()
+  protected var sinks = Seq(sink)
+
   def get(): T = executor.execute(sinks.head, Map()).asInstanceOf[DatumExpression].get.asInstanceOf[T]
 }
 
 // A lazy representation of a pipeline output
-class PipelineDatasetOut[T] extends GraphBackedExecution {
+class PipelineDatasetOut[T](protected var executor: GraphExecutor, sink: SinkId) extends GraphBackedExecution {
+  protected var sources = Seq()
+  protected var sinks = Seq(sink)
+
   def get(): RDD[T] = executor.execute(sinks.head, Map()).asInstanceOf[DatasetExpression].get.asInstanceOf[RDD[T]]
 }
 
-abstract class Pipeline[A, B] {
+class Pipeline[A, B](protected var executor: GraphExecutor, source: SourceId, sink: SinkId) {
   // These three vars point to the internal representation of this pipeline
+  protected var sources = Seq(source)
+  protected var sinks = Seq(sink)
 
-  def apply(datum: A): PipelineDatumOut[B]
-  def apply(data: RDD[A]): PipelineDatasetOut[B]
+  def apply(datum: A): PipelineDatumOut[B] = {
+    val (graphWithDatum, datumId) = executor.graph.addNode(new DatumOperator(datum), Seq())
+    val newGraph = graphWithDatum.replaceDependency(sources.head, datumId)
+      .removeSource(sources.head)
+
+    new PipelineDatumOut[B](new GraphExecutor(newGraph, executor.optimizer), sinks.head)
+  }
+
+  def apply(data: RDD[A]): PipelineDatasetOut[B] = {
+    val (graphWithDatum, datumId) = executor.graph.addNode(new DatasetOperator(data), Seq())
+    val newGraph = graphWithDatum.replaceDependency(sources.head, datumId)
+      .removeSource(sources.head)
+
+    new PipelineDatasetOut[B](new GraphExecutor(newGraph, executor.optimizer), sinks.head)
+  }
+
   def apply(data: PipelineDatasetOut[A]): PipelineDatasetOut[B]
 }
 
@@ -81,9 +102,31 @@ abstract class LabelEstimator[A, B, L] {
   def fit(data: RDD[A], labels: RDD[L]): Pipeline[A, B]
 }
 
-object Pipeline {
+object GraphBackedExecution {
   // Combine all the internal graph representations to use the same, merged representation
-  def tie(outs: GraphBackedExecution*): Unit = ???
+  def tie(graphBackedExecutions: Seq[GraphBackedExecution], optimizer: Option[Optimizer]): Unit = {
+    val emptyGraph = new Graph(Set(), Map(), Map(), Map())
+    val (newGraph, sourceMappings, sinkMappings) = graphBackedExecutions.foldLeft(
+      emptyGraph,
+      Seq[Map[SourceId, SourceId]](),
+      Seq[Map[SinkId, SinkId]]()
+    ) {
+      case ((curGraph, curSourceMappings, curSinkMappings), graphExecution) =>
+        val (nextGraph, nextSourceMapping, nextSinkMapping) = curGraph.addGraph(graphExecution.executor.graph)
+        (nextGraph, curSourceMappings :+ nextSourceMapping, curSinkMappings :+ nextSinkMapping)
+    }
+
+    val newExecutor = new GraphExecutor(graph = newGraph, optimizer = optimizer)
+    for (i <- graphBackedExecutions.indices) {
+      val execution = graphBackedExecutions(i)
+      execution.executor = newExecutor
+      execution.sources = execution.sources.map(sourceMappings(i))
+      execution.sinks = execution.sinks.map(sinkMappings(i))
+    }
+  }
+}
+
+object Pipeline {
 
   // If the checkpoint is found, return an output that just reads it from disk.
   // If the checkpoint is not found, return the input data graph w/ an EstimatorOperator just saves to disk added at the end

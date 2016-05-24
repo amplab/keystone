badfba775b3a64cc8e718872b07be0de99e3c808
Tomer Kaftan <tomer.kaftan@gmail.com>
2016-05-24 14:05:03 -0700

    moved autocache stuff out of this branch

--- src/main/scala/workflow/graph/WeightedOperator.scala
+++
@@ -1,9 +0,0 @@
-package workflow.graph
-
-/**
- * A mix-in that attaches a weight to an operator that represents how often it must iterate
- * over its input.
- */
-trait WeightedOperator {
-  val weight: Int
-}

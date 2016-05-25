2212848eda1e851abe56833cbaa029fbbf7d4818
Tomer Kaftan <tomer.kaftan@gmail.com>
2016-05-24 17:34:23 -0700

    more code cleanup

--- src/main/scala/workflow/graph/Pipeline.scala
+++ src/main/scala/workflow/graph/Pipeline.scala
@@ -6,7 +6,7 @@ import scala.reflect.ClassTag
 
 private[graph] class GraphExecutor(graph: Graph, state: Map[GraphId, Expression], optimize: Boolean = true) {
   private var optimized: Boolean = false
-  private lazy val optimizedState: (Graph, scala.collection.mutable.Map[GraphId, Expression]) = {
+  private lazy val optimizedGraphAndState: (Graph, scala.collection.mutable.Map[GraphId, Expression]) = {
     val (newGraph, newExecutionState) = if (optimize) {
       Pipeline.getOptimizer.execute(graph, state)
     } else {
@@ -16,8 +16,8 @@ private[graph] class GraphExecutor(graph: Graph, state: Map[GraphId, Expression]
     optimized = true
     (newGraph, scala.collection.mutable.Map() ++ newExecutionState)
   }
-  private lazy val optimizedGraph: Graph = optimizedState._1
-  private lazy val executionState: scala.collection.mutable.Map[GraphId, Expression] = optimizedState._2
+  private lazy val optimizedGraph: Graph = optimizedGraphAndState._1
+  private lazy val optimizedState: scala.collection.mutable.Map[GraphId, Expression] = optimizedGraphAndState._2
 
   def getGraph: Graph = if (optimized) {
     optimizedGraph
@@ -26,64 +26,43 @@ private[graph] class GraphExecutor(graph: Graph, state: Map[GraphId, Expression]
   }
 
   def getState: Map[GraphId, Expression] = if (optimized) {
-    executionState.toMap
+    optimizedState.toMap
   } else {
     state
   }
 
   // Todo put comment: A result is unstorable if it implicitly depends on any source
-  private lazy val unstorableResults: Set[GraphId] = {
+  private lazy val descendantsOfSources: Set[GraphId] = {
     optimizedGraph.sources.foldLeft(Set[GraphId]()) {
       case (descendants, source) => descendants ++ AnalysisUtils.getDescendants(optimizedGraph, source) + source
     }
   }
 
   // TODO rename and comment. This method executes & stores all ancestors of a sink that don't depend on sources, and haven't already been executed
-  def executeAndSaveWithoutSources(sink: SinkId): Unit = {
-    val linearizedAncestors = AnalysisUtils.linearize(optimizedGraph, sink)
-    val ancestorsToExecute = linearizedAncestors.filter(id => (!unstorableResults.contains(id)) && (!executionState.contains(id)))
-    ancestorsToExecute.foreach {
-      case source: SourceId => throw new RuntimeException("Linearized ancestors to execute should not contain sources")
-      case node: NodeId => {
-        val dependencies = optimizedGraph.getDependencies(node)
-        val depExpressions = dependencies.map(dep => executionState(dep))
-        val operator = optimizedGraph.getOperator(node)
-        executionState(node) = operator.execute(depExpressions)
-      }
-    }
-
-    val sinkDep = optimizedGraph.getSinkDependency(sink)
-    if (ancestorsToExecute.contains(sinkDep) && (!executionState.contains(sink))) {
-      executionState(sink) = executionState(sinkDep)
-    }
+  def partialExecute(graphId: GraphId): Unit = {
+    val linearizedAncestors = AnalysisUtils.linearize(optimizedGraph, graphId) :+ graphId
+    val ancestorsToExecute = linearizedAncestors.filter(id => (!descendantsOfSources.contains(id)) && (!optimizedState.contains(id)))
+    ancestorsToExecute.foreach {ancestor => execute(ancestor)}
   }
 
-  private def getUncachedResult(graphId: GraphId, sources: Map[SourceId, Expression]): Expression = {
-    graphId match {
-      case source: SourceId => sources.get(source).get
-      case node: NodeId => {
-        val dependencies = optimizedGraph.getDependencies(node)
-        val depExpressions = dependencies.map(dep => getResult(dep, sources))
-        val operator = optimizedGraph.getOperator(node)
-        operator.execute(depExpressions)
-      }
-      case sink: SinkId => {
-        val sinkDep = optimizedGraph.getSinkDependency(sink)
-        getResult(sinkDep, sources)
-      }
-    }
-  }
+  def execute(graphId: GraphId): Expression = {
+    require(!descendantsOfSources.contains(graphId), "May not execute GraphIds dependent on unconnected sources.")
 
-  private def getResult(graphId: GraphId, sources: Map[SourceId, Expression]): Expression = {
-    if (unstorableResults.contains(graphId)) {
-      getUncachedResult(graphId, sources)
-    } else {
-      executionState.getOrElseUpdate(graphId, getUncachedResult(graphId, sources))
-    }
-  }
-
-  def execute(sinkId: SinkId, sources: Map[SourceId, Expression]): Expression = {
-    getResult(sinkId, sources)
+    optimizedState.getOrElseUpdate(graphId, {
+      graphId match {
+        case source: SourceId => throw new IllegalArgumentException("Unconnected sources have no stored value")
+        case node: NodeId => {
+          val dependencies = optimizedGraph.getDependencies(node)
+          val depExpressions = dependencies.map(dep => execute(dep))
+          val operator = optimizedGraph.getOperator(node)
+          operator.execute(depExpressions)
+        }
+        case sink: SinkId => {
+          val sinkDep = optimizedGraph.getSinkDependency(sink)
+          execute(sinkDep)
+        }
+      }
+    })
   }
 }
 
@@ -117,7 +96,7 @@ abstract class GraphBackedExecution[T](
   private var ranExecution: Boolean = false
   private lazy val finalExecutor: GraphExecutor = {
     if (getSources.nonEmpty) {
-      getExecutor.executeAndSaveWithoutSources(getSink)
+      getExecutor.partialExecute(getSink)
 
       val unmergedGraph = _sources.foldLeft(getExecutor.getGraph) {
         case (curGraph, (sourceId, sourceOp)) => {
@@ -154,7 +133,7 @@ abstract class GraphBackedExecution[T](
     getExecutor.getState
   }
 
-  final def get(): T = expressionToOutput(finalExecutor.execute(getSink, Map()))
+  final def get(): T = expressionToOutput(finalExecutor.execute(getSink))
 }
 
 // A lazy representation of a pipeline output
@@ -257,6 +236,15 @@ trait Pipeline[A, B] {
 
 }
 
+/**
+ * An implementation of [[Pipeline]] that explicitly specifies the Pipline internals as vals.
+ *
+ * @param executor The [[GraphExecutor]] underlying the Pipeline execution.
+ * @param source The SourceId of the pipeline
+ * @param sink The SinkId of the Pipeline
+ * @tparam A The type of input to the Pipeline
+ * @tparam B The type of output from the Pipeline
+ */
 private[graph] class ConcretePipeline[A, B](
   override private[graph] val executor: GraphExecutor,
   override private[graph] val source: SourceId,

d86fddec8c6a8058286fa0a0bf1983386fcdd94a
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2016-06-05 15:31:53 -0700

    Fix broken build by adding ClassTag to ChainUtils

--- src/main/scala/workflow/graph/ChainUtils.scala
+++ src/main/scala/workflow/graph/ChainUtils.scala
@@ -2,12 +2,14 @@ package workflow.graph
 
 import org.apache.spark.rdd.RDD
 
+import scala.reflect.ClassTag
+
 /**
  * A chain of two Transformers in a row (as a Transformer)
  * @param first
  * @param second
  */
-case class TransformerChain[A, B, C](first: Transformer[A, B], second: Transformer[B, C]) extends Transformer[A, C] {
+case class TransformerChain[A, B, C : ClassTag](first: Transformer[A, B], second: Transformer[B, C]) extends Transformer[A, C] {
   override def apply(in: A): C = second(first(in))
   override def apply(in: RDD[A]): RDD[C] = second(first(in))
 }
@@ -17,7 +19,7 @@ case class TransformerChain[A, B, C](first: Transformer[A, B], second: Transform
  * @param first
  * @param second
  */
-case class TransformerEstimatorChain[A, B, C](first: Transformer[A, B], second: Estimator[B, C])
+case class TransformerEstimatorChain[A, B, C : ClassTag](first: Transformer[A, B], second: Estimator[B, C])
   extends Estimator[A, C] {
 
   override def fit(data: RDD[A]): Transformer[A, C] = {
@@ -30,7 +32,7 @@ case class TransformerEstimatorChain[A, B, C](first: Transformer[A, B], second: 
  * @param first
  * @param second
  */
-case class TransformerLabelEstimatorChain[A, B, C, L](first: Transformer[A, B], second: LabelEstimator[B, C, L])
+case class TransformerLabelEstimatorChain[A, B, C : ClassTag, L](first: Transformer[A, B], second: LabelEstimator[B, C, L])
   extends LabelEstimator[A, C, L] {
 
   override def fit(data: RDD[A], labels: RDD[L]): Transformer[A, C] = {

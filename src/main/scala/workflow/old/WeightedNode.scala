d351b37e69be2c8d6a5260335c98cf628c4f5fcb
Evan Sparks <sparks@cs.berkeley.edu>
2016-06-06 23:12:28 -0700

    Removing last traces of old backend.

--- src/main/scala/workflow/old/WeightedNode.scala
+++
@@ -1,9 +0,0 @@
-package workflow.old
-
-/**
- * A mix-in that attaches a weight to a node that represents how often it must iterate
- * over its input.
- */
-trait WeightedNode {
-  val weight: Int
-}
